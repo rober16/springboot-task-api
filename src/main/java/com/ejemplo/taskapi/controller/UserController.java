63893dca5187c3cfb8113b4d2702bc057a8626b2
Roberto Gomez <rgomezmartearena@ubp.edu.ar>
2026-04-27 18:46:28 -0300

    Se creo UserController

---
+++ src/main/java/com/ejemplo/taskapi/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.ejemplo.taskapi.controller;
+
+import com.ejemplo.taskapi.dto.request.UserCreateRequest;
+import com.ejemplo.taskapi.dto.response.TaskResponse;
+import com.ejemplo.taskapi.dto.response.UserResponse;
+import com.ejemplo.taskapi.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping
+    public UserResponse create(@Valid @RequestBody UserCreateRequest request) {
+        return userService.create(request);
+    }
+
+    @GetMapping
+    public List<UserResponse> findAll() {
+        return userService.findAll();
+    }
+}

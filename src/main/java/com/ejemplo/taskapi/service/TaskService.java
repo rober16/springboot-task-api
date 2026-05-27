4cdd5054d6b7ec04e3229af258b39230a5323ea4
Roberto Gomez <rgomezmartearena@ubp.edu.ar>
2026-05-26 21:26:24 -0300

    Se crearon nuevos metodos

--- src/main/java/com/ejemplo/taskapi/service/TaskService.java
+++ src/main/java/com/ejemplo/taskapi/service/TaskService.java
@@ -3,6 +3,7 @@ package com.ejemplo.taskapi.service;
 import com.ejemplo.taskapi.dto.request.TaskCreateRequest;
 import com.ejemplo.taskapi.dto.request.TaskUpdateRequest;
 import com.ejemplo.taskapi.dto.response.TaskResponse;
+import com.ejemplo.taskapi.model.enums.TaskStatus;
 
 import java.util.List;
 
@@ -13,6 +14,10 @@ public interface TaskService {
     List<TaskResponse> findAll();
 
     TaskResponse findById(Long id);
+
+    List<TaskResponse> findByStatus(TaskStatus status);
+
+    List<TaskResponse> findByUserId(Long userId);
 
     TaskResponse update(Long id, TaskUpdateRequest request);
 
